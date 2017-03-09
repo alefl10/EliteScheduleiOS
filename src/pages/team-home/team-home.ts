c9c20d1263c5e9be7be5fe5ccd2c7cd01e1f39b5
System Administrator <root@Alejandros-MacBook-Pro.local>
2017-03-09 14:28:54 -0500

    Elite Schedule App ready for iOS

---
+++ src/pages/team-home/team-home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { MyTeamsPage, TeamDetailPage, StandingsPage} from '../pages';
+
+@Component({
+  selector: 'page-team-home',
+  templateUrl: 'team-home.html'
+})
+export class TeamHomePage {
+
+  team: any;
+  teamDetailTab = TeamDetailPage;
+  standingsTab = StandingsPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.team = this.navParams.data
+  }
+
+  goHome(){
+    //this.navCtrl.push(MyTeamsPage);
+    this.navCtrl.popToRoot();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TeamHomePage');
+  }
+
+}
